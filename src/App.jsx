c6f226d8709c03f01c245b78e84c48a9b93b090b
abdelmuttalib <hagar9886@gmail.com>
2022-02-25 23:17:06 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,9 @@ import React from 'react'
 function App() {
   return (
     <div className="min-h-screen w-full flex flex-col gap-10 justify-center items-center">
-      <h1 className="text-2xl flex-wrap sm:text-4xl lg:text-5xl lg:font-bold flex justify-center gap-3 text-white italic font-medium">
-        <span className="text-yellow-400">Vite</span>
-        <span className="text-cyan-500">Tailwind</span>
-        <span className="text-purple-500">Eslint</span>
-        <span className="text-pink-500 italic">Airbnb Styleguide</span>
-        <span className="text-black font-normal">Starter</span>
+      <h1 className="text-3xl flex-wrap sm:text-4xl lg:text-5xl lg:font-bold flex justify-center gap-3 text-white italic font-medium">
+        <span className="text-black font-semibold">Photo Gallery App</span>
       </h1>
-      <a
-        href="https://github.com/Abdelmuttalib"
-        className="text-black hover:text-opacity-80 font-medium"
-      >
-        Abdelmuttalib Ahmed
-      </a>
     </div>
   )
 }
